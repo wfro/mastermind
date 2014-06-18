f74f2336f9f7e00c76bd146c5010cbefa1539eac
Will Faurot <wfaurot@gmail.com>
2014-06-18 12:39:33 -0600

    Add menu.

--- lib/game.rb
+++ lib/game.rb
@@ -2,8 +2,8 @@
 # Keep timer
 # Store history (later feature)
 #
-# What else will game do?  Create functions here and decide where to put them?
-# What class is going to do actual execution?
+# What else will game do?  Create functions here and decide where to put them
+# Where am I going to create all of the objects and actually execute the code
 
 
 class Game
@@ -19,6 +19,26 @@ class Game
 
   def <<(guess)
     @guesses << guess
+  end
+
+  def start
+  
+  end
+
+
+  def menu
+    # quit, play, instructions
+    puts "Intro message placeholder"
+    command = ''
+    while command != 'q'
+      # get input and
+      command = gets.chomp
+      case command
+      when 'q' then puts "Goodbye.  Make this clever eventually."
+      when 'p' then # start game loop
+      when 'i' then instructions
+      end
+    end
   end
 
 end
